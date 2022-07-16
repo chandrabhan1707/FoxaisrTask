b2186288a11d870c1c38a4c81b9a05e9e909b439
chandrabhan1707 <chandrabhan1707@gmail.com>
2022-07-16 17:06:27 +0530

    Add files via upload

---
+++ CRUDAPP/urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path
+from CrudApp import views
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+    re_path(r'^department$',views.departmentApi),
+    re_path(r'^department/([0-9]+)$',views.departmentApi),
+
+    re_path(r'^employee$',views.employeeApi),
+    re_path(r'^employee/([0-9]+)$',views.employeeApi)
+]
